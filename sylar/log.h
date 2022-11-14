d009170e98df259eed1626e23de45e8baea5803b
sliver <396803498@qq.com>
2022-11-14 11:37:18 +0000

    LogSystem04_formatter

--- sylar/log.h
+++ sylar/log.h
@@ -15,6 +15,14 @@ class LogEvent {
 public:
     typedef std::shared_ptr<LogEvent> ptr;
     LogEvent();
+
+    const char* getFile() const { return m_file;}
+    int32_t getLine() const { return m_file;}
+    uint32_t getElapse() const { return m_elapse;}
+    uint32_t getThreadId() const { return m_threadId;}
+    uint32_t getFiberId() const { return m_fiberId;}
+    uint64_t getTime() const { return m_time;}
+    const std::string& getContent() const { return m_content;} 
 private:
     const char* m_file = nullptr;//文件名
     int32_t m_line = 0;          //行号
@@ -29,21 +37,36 @@ private:
 class LogLevel {
 public:
     enum Level {
+        UNKNOW = 0,
         DEBUG = 1,
         INFO = 2,
         WARN = 3,
         ERROR = 4,
         FATAL = 5
     };
+
+    static const char* ToString(LogLevel::Level level);
 };
 
 //日志格式器
 class LogFormatter {
 public:
     typedef std::shared_ptr<LogFormatter> ptr;
+    LogFormatter(const std::string& pattern);
 
-    std::string format(LogEvent::ptr event);
+    std::string format(LogLevel::Level level, LogEvent::ptr event);
+public:
+    class FormatItem {
+    public:
+        typedef std::shared_ptr<FormatItem> ptr;
+        virtual ~FormatItem() {};
+        virtual void format(std::ostream& os,LogLevel::Level level, LogEvent::ptr event) = 0;
+    };
+
+    void init();
 private:
+    std::string m_pattern;
+    std::vector<FormatItem::ptr> m_items;
 };
 
 
