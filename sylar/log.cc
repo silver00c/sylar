a4d068917e9f38223ea0db0280d00b24e543d07d
sliver <396803498@qq.com>
2022-11-14 06:25:51 +0000

    LogSystem02_logger

--- sylar/log.cc
+++ sylar/log.cc
@@ -1,83 +1,49 @@
-#ifndef __SYLAR_LOG_H__
-#define __SYLAR_LOG_H__
+#include "log.h"
 
-#include <string>
-#include <stdint.h>
-#include <memory>
-
-namespace sylar{
-
-//日志事件
-class LogEvent {
-public:
-    typedef std::shared_ptr<LogEvent> ptr;
-    LogEvent();
-private:
-    const char* m_file = nullptr;//文件名
-    int32_t m_line = 0;          //行号
-    uint32_t m_elapse = 0        //程序启动开始到现在的毫秒数
-    uint32_t m_threadId = 0;     //线程id
-    uint32_t m_fiberId = 0;      //协程id
-    uint64_t m_time = 0;         //时间戳
-    std::string m_content;
-};
-
-//日志级别
-class LogLevel {
-public:
-    enum Level {
-        DEBUG = 1,
-        INFO = 2,
-        WARN = 3,
-        ERROR = 4,
-        FATAL = 5
-    };
-};
-
-//日志格式器
-class LogFormatter {
-public:
-    typedef std::shared_ptr<LogFormatter> ptr;
-
-    std::string format(LogEvent::ptr event);
-private:
-};
-
-
-//日志输出地
-class LogAppender {
-public:
-    typedef std::shared_ptr<LogAppender> ptr;
-    virtual ~LogAppender() {};
-
-    void log(LogLevel::Level level, LogEvent::ptr event);
-private:
-    LogLevel::Level m_level;
-};
-
-
-//日志器
-class Logger {
-public:
-    typedef std::shared_ptr<Logger> ptr;
-    
-    Logger(const std::string& name = "root");
-
-    void log(LogLevel::Level level, LogEvent::ptr event);
-private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    LogAppender::ptr;
-};
-
-//输出到控制台的Appender
-class StdoutLogAppender : public LogAppender {
-};
-
-//定义输出到文件的Appender
-class FileLogAppender : public LogAppender {
-};
-
+namespace sylar {
+Logger::Logger(const std::string& name)
+    :m_name(name) {
 }
 
-#endif
+void Logger::addAppender (LogAppender::ptr appender) {
+    m_appenders.push_back(appender);
+}
+
+void Logger::delAppender (LogAppender::ptr appender) {
+    for(auto it = m_appenders.begin(); it != m_appenders.end(); ++it){
+        if(*it == appender) {
+            m_appenders.erase(it);
+            break;
+        }
+    }
+}
+
+void Logger::log(LogLevel::Level level, LogEvent::ptr event) {
+    if(level >= m_level) {
+        for(auto& i : m_appenders) {
+            i->log(level, event);
+        }
+    }
+}
+
+void Logger::debug(LogEvent::ptr event) {
+    debug(LogLevel::DEBUG, event);
+}
+
+void Logger::info(LogEvent::ptr event) {
+    debug(LogLevel::INFO, event);
+}
+
+void Logger::warn(LogEvent::ptr event) {
+    debug(LogLevel::WARN, event);
+}
+
+void Logger::error(LogEvent::ptr event) {
+    debug(LogLevel::ERROR, event);
+}
+
+void Logger::fatal(LogEvent::ptr event) {
+    debug(LogLevel::FATAL, event);
+}
+
+};
